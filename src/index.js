13ba42db8c2cd1e56d1fd0887461958ae30d0fc3
Hanan Dlshad <ensw03217@student.su.edu.krd>
2020-05-31 15:26:42 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-console.log('%c HI', 'color: firebrick')
+console.log('%c HI', 'color: firebrick');
+//Challenge 1
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+let divImage = document.getElementById("dog-image-container");
+fetch(imgUrl).then(function(response){
+  return response.json();
+}).then(function(json) {
+  let imgDiv = document.getElementById("dog-image-container");
+  for(const element of json.message){
+    let imge=document.createElement('img');
+    imge.setAttribute('element');
+    imgDiv.appendChild(imge);
+  }
+});
+//Challenge 2
+const breedUrl = 'https://dog.ceo/api/breeds/list/all'
